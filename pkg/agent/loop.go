cd33a6df42d6680ea236282a17ca766dc49fb10f
Paul De Velder <paul.develder@attentia.be>
2026-02-25 16:33:48 +0100

    chore: remove nanobot references, rebrand skill metadata to millibee

--- pkg/agent/loop.go
+++ pkg/agent/loop.go
@@ -1,5 +1,4 @@
 // MilliBee - Ultra-lightweight personal AI agent
-// Inspired by and based on nanobot: https://github.com/HKUDS/nanobot
 // License: MIT
 //
 // Copyright (c) 2026 MilliBee contributors
